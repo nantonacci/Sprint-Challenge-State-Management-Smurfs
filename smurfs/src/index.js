35478dc0cec24aeaa1cbc105402bdf9cbce81b50
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-09-28 09:21:46 -0700

    Redux set up. Index ready.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+//import { reducer } from './reducers';
+
+const reducer = () => {
+  return 'itsa me, reducer!';
+};
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
